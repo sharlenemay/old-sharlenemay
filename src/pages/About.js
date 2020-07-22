041fdecc3c2f4f3bcec83c1cdab1641a027956b2
Sharlene May <sharleneminosa@gmail.com>
2020-07-21 21:50:23 -0700

    updated about

--- src/pages/About.js
+++ src/pages/About.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     margin: "5%",
   },
   h3: {
-    fontStyle: "italic"
+    fontStyle: "italic",
   },
   button: {
     backgroundColor: "black",
@@ -45,7 +45,7 @@ export default function About() {
               <h3 className={classes.h3}>
                 My mission in life is not merely to survive, but to thrive; and
                 to do so with some passion, some compassion, some humor, and
-                some style. <br/> - Maya Angelou
+                some style. <br /> - Maya Angelou
               </h3>
               <h4>About Me</h4>
               <p>
@@ -70,28 +70,21 @@ export default function About() {
           </Grid>
           <Grid item xs={12} sm={12} md={6} lg={6}>
             <Paper className={classes.paper}>
-              <h4>Coding Languages and Frameworks</h4>
+              <h4>Web Development</h4>
               <p>
-                HTML, CSS, JavaScript, Node, React, Bootstrap, Material-UI,
-                Handlebars
-              </p>
-              <h4>World Languages</h4>
-              <p>
-                English Native <br />
-                Japanese Proficient <br />
-                Elementary Korean <br />
-                Elementary French
+                I'm a MERN full stack web developer with my specialization in front end development.
               </p>
               <h4>Education</h4>
               <p>
-                Boston University, College of Communication, Master of Arts
-                Candidate in Emerging Media Studies - expected Graduation May
-                2022 <br />
-                UCLA Extension, Bootcamp Certificate: 12-Week Full-stack Web
-                Development Program - July 2020 <br />
-                University of California, Los Angeles (UCLA), Bachelor of Arts
-                in Anthropology and Bachelor of Arts in Asian Humanities - Magna
-                Cum Laude 2018
+                Boston University, College of Communication (BU COM)
+                <br /> Master of Arts Candidate in Emerging Media Studies -
+                expected Graduation May 2022
+              </p>
+              <p>
+                University of California, Los Angeles (UCLA)
+                <br />
+                Double Bachelor of Arts in Anthropology and in Asian Humanities
+                - Magna Cum Laude 2018
               </p>
             </Paper>
           </Grid>
